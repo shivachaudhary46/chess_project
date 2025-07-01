d0f8dc0f15ca1209f63b49f17ecc82abfd4a65a2
shivachaudhary46 <shivachaudhary4116@gmail.com>
2025-07-02 05:24:58 +0545

    i forgot counting days but its been i guess 14 days i started chess projecct

--- chessEngine1.py
+++ chessEngine1.py
@@ -16,16 +16,238 @@ class gamestate:
                     ['--', '--', '--', '--', '--', '--', '--', '--'],
                     ['wP', 'wP', 'wP', 'wP', 'wP', 'wP', 'wP', 'wP'],
                     ['wR', 'wN', 'wB', 'wQ', 'wK',  'wB', 'wN', 'wR']]
+        self.directions = {
+                "R": [[1, 0], [-1, 0], [0, 1], [0, -1]],
+                "N": [[-2, -1], [-2, 1], [2, -1], [1, 2], [-1, 2], [2, 1], [-1, -2], [1, -2]], 
+                "B": [[1, 1], [1, -1], [-1, 1], [-1, -1]], 
+                "Q": [[1, 0], [-1, 0], [0, 1], [0, -1], [1, 1], [1, -1], [-1, -1], [-1, 1]], 
+                "K": [[+1, 0], [-1, 0], [0, +1], [0, -1], [+1, +1], [-1, -1], [+1, -1], [-1, +1]]
+            }
         self.isWhiteTurn = True
         self.moveLog = []
 
+    def makeMove(self, move):
+        self.board[move.startRow][move.startCol] = '--'
+        self.board[move.endRow][move.endCol] = move.pieceSource
+        self.moveLog.append(move) #log the move 
+        self.isWhiteTurn = not self.isWhiteTurn
+        
+    '''print a board state'''
+    def printBoardState(self):
+        print("   a  b  c  d  e  f  g  h")
+        print()
+        for i, row in enumerate(self.board):
+            print(f'{8-i}  {" ".join(row)}  {8-i}')
+
+        print()
+        print("   a  b  c  d  e  f  g  h")
+
+    '''undo moving the pieces'''
+    def undoMove(self):
+        if len(self.moveLog) != 0:
+            move = self.moveLog.pop()
+            self.board[move.startRow][move.startCol] = move.pieceSource
+            self.board[move.endRow][move.endCol] = move.pieceDest
+            self.isWhiteTurn = not self.isWhiteTurn
+
+    '''generate all possible moves for each piece'''
+    def getAllValidMoves(self):
+        moves=[]
+        for r in range(len(self.board)):
+            for c in range(len(self.board[r])):
+                turn=self.board[r][c][0]
+                if (turn == 'w' and self.isWhiteTurn) or (turn == 'b' and not self.isWhiteTurn):
+                    piece = self.board[r][c][1]
+                    if piece == 'P':
+                        self.getPawnMoves(turn, r, c, moves)
+
+                    elif piece == 'N':
+                        self.getKnightMoves(r, c, moves)
+
+                    elif piece == 'R':
+                        self.getRookMoves(r, c, moves)
+
+                    elif piece == 'B':
+                        self.getBishopMoves(r, c, moves)
+
+                    elif piece == 'Q':
+                        self.getQueenMoves(r, c, moves)
+
+                    elif piece == 'K':
+                        self.getKingMoves(r, c, moves)
+        
+        return moves
+
+    '''get all pawn moves at a specific row and col in board state'''                                        
+    def getPawnMoves(self, turn, r, c, moves):
+        '''if white pawn then'''
+        if self.isWhiteTurn:
+            if r > 0 and self.board[r-1][c] == '--':
+                moves.append(Move((r, c), (r-1, c), self.board))
+
+            if r == 6 and self.board[r-2][c] == '--':
+                moves.append(Move((r, c), (r-2, c), self.board))
+
+            for diag in [-1, 1]:
+                nx, ny = r - 1, c+diag
+                if 0<=nx<=7 and 0<=ny<=7 :
+                    target=self.board[nx][ny]
+                    
+                    if target[0] == 'b':
+                        moves.append(Move((r, c), (nx, ny), self.board))
+    
+        if not self.isWhiteTurn:
+            '''if black pawn then'''
+            if r < 7 and self.board[r+1][c] == '--':
+                moves.append(Move((r, c), (r+1, c), self.board))
+
+            if r == 1 and self.board[r+2][c] == '--':
+                moves.append(Move((r, c), (r+2, c), self.board))
+
+            for diag in [-1, 1]:
+                nx, ny = r + 1, c+diag
+                if 0<=nx<=7 and 0<=ny<=7 :
+                    target=self.board[nx][ny]
+                    
+                    if target[0] == 'w':
+                        moves.append(Move((r, c), (nx, ny), self.board))
+
+    '''get all knight moves at a specific row and col in board state'''
+    def getKnightMoves(self, r, c, moves):
+        for direction in self.directions['N']:
+            nx, ny = r + direction[0], c + direction[1]
+            if 0<=nx<=7 and 0<=ny<=7:
+                target=self.board[nx][ny]
+                if target == '--' or (target[0] == 'b' and self.isWhiteTurn) or (target[0] == 'w' and not self.isWhiteTurn):
+                    moves.append(Move((r, c), (nx, ny), self.board))
+    
+    '''get all rook moves at a specific row and col in board state'''
+    def getRookMoves(self, r, c, moves):
+        for direction in self.directions['R']:
+            for step in range(1, 8):
+                nx, ny = r+step*direction[0], c+step*direction[1]
+                
+                ## the moving piece destination should be limit inside the 
+                ## 8 * 8 array
+                if 0<=nx<=7 and 0<=ny<=7:
+                    target = self.board[nx][ny]
+
+                    # if destination is empty 
+                    if target == '--':
+                        moves.append(Move((r, c), (nx, ny), self.board))
+
+                    # capturing piece
+                    elif ((target[0] == 'b' and self.isWhiteTurn) or (target[0] == 'w' and not self.isWhiteTurn)):
+                        moves.append(Move((r, c), (nx, ny), self.board))
+                        break
+                    
+                    ## target is same as one 
+                    else:
+                        break
+                else:
+                    break
+
+    '''get all Queen moves at a specific row and col in board state'''
+    def getQueenMoves(self, r, c, moves):
+        enemyColor = 'b' if self.isWhiteTurn else 'w'
+        for direction in self.directions['Q']:
+            for step in range(1, 8):
+                nx, ny = r+step*direction[0], c+step*direction[1]
+                
+                '''new row and new col must be inside 8*8 2D board visualization'''
+                if 0<=nx<=7 and 0<=ny<=7:
+                    target = self.board[nx][ny]
+
+                    '''if destination is empty'''
+                    if target == '--':
+                        moves.append(Move((r, c), (nx, ny), self.board))
+
+                    elif (target[0] == enemyColor):
+                        '''How can i capture pieces ??'''
+                        moves.append(Move((r, c), (nx, ny), self.board))
+                        break
+                    else:
+                        '''Target same as the pieces'''
+                        break
+                else:
+                    break
+            
+
+    '''get all king moves at a specific row and col'''
+    def getKingMoves(self, r, c, moves):
+        for direction in self.directions['K']:
+            nx, ny = r + direction[0], c + direction[1]
+            if 0<=nx<=7 and 0<=ny<=7:
+                target=self.board[nx][ny]
+                if target == '--' or (target[0] == 'b' and self.isWhiteTurn) or (target[0] == 'w' and not self.isWhiteTurn):
+                    moves.append(Move((r, c), (nx, ny), self.board))
+
+    '''get all bishop moves at a specific row and col'''
+    def getBishopMoves(self, r, c, moves):
+        for direction in self.directions['B']:
+            for step in range(1, 8):
+                nx, ny = r+step*direction[0], c+step*direction[1]
+                
+                ## the moving piece destination should be limit inside the 
+                ## 8 * 8 array
+                if 0<=nx<=7 and 0<=ny<=7:
+                    target = self.board[nx][ny]
+
+                    # if destination is empty 
+                    if target == '--':
+                        moves.append(Move((r, c), (nx, ny), self.board))
+
+                    # capturing piece
+                    elif ((target[0] == 'b' and self.isWhiteTurn) or (target[0] == 'w' and not self.isWhiteTurn)):
+                        moves.append(Move((r, c), (nx, ny), self.board))
+                        break
+                    
+                    ## target is same as one 
+                    else:
+                        break
+                else:
+                    break
+
+    def isPlayerValidMove(self, move, validMoves):
+        iterate_valid = []
+        for each in validMoves:
+            iterate_valid.append(((each.startRow, each.startCol), (each.endRow, each.endCol)))
+        
+        print(iterate_valid)
+        source = move.startRow, move.startCol
+        dest = move.endRow, move.endCol
+        piece = (source, dest)
+        return piece in iterate_valid
+
 class numericalBoard(gamestate):
+
+    ''' text format is encoded 
+        later we have to convert to nnumerical board while doing the model training for chess Engine 
+    '''
+    encoded = {"wR": 2 , "wN": 3, "wB": 4, "wQ": 5, "wK": 6, "wP": 1, "bR": -2, "bN": -3, "bB": -4, "bQ": -5, "bK": -6, "bP": -1, "--": 0}
     def __init__(self):
         '''
         i want to convert the string board to numerical board. so, we can do most of the 
         operations easily  
         '''
+        super().__init__()
 
+    '''adding decorator so we don't have to add self argument '''
+    @staticmethod 
+    def to_numericalboard(board, encoding):
+        ''' Chess has 2D array board view. so we convert numerical_board representation later on if 
+        before creating a new model'''
+        num_board = []
+        for row in board:
+            each_rows = []
+            for c in row:
+                each_rows.append(encoding.get(c, 0))
+            num_board.append(each_rows)
+        
+        return num_board
+    
+    def get_numericalboard(self):
+        return self.to_numericalboard(self.board, self.encoded)
 
 
 class Move:
@@ -33,11 +255,6 @@ class Move:
         it will have the startSource and destination converted as row and columns
         maps key and values as rank to rows 
     '''
-    rankToRows = {'1':7, '2':6, '3':5, '4':4, '5':3, '6':2, '7':1, '8':0}
-    rowsToRanks = {v: k for k, v in rankToRows.items()}
-    rankToCols = {'a':0, 'b':1, 'c':2, 'd':3, 'e':4, 'f':5, 'g':6, 'h':7}
-    colsToFiles = {v:k for k, v in rankToCols.items()}
-
     def __init__(self, startSq, endSq, board):
         ''' basically it will divide start source and ending destination (row, col) as seperate row and col'''
         self.startRow = startSq[0]
@@ -46,6 +263,31 @@ class Move:
         self.endCol = endSq[1]
         self.pieceSource = board[self.startRow][self.startCol]
         self.pieceDest = board[self.endRow][self.endCol]
+        self.moveId = self.startRow * 1000 + self.startCol * 100 + self.endRow * 10 + self.endCol
+    '''
+    prints like source to destination of pieces 
+    '''
+    def __str__(self):
+        print(f"Move: {self.pieceSource} from ({self.startRow}, {self.startCol}) to ({self.endRow}, {self.endCol})")
+
+    def __eq__(self, other):
+        if isinstance(other, Move):
+            return self.moveId == other.moveId
+        return False
+
+
+'''
+    it will have the startSource and destination converted as row and columns
+    maps key and values as rank to rows 
+'''
+class Conversion:
+    rankToRows = {'1':7, '2':6, '3':5, '4':4, '5':3, '6':2, '7':1, '8':0}
+    rowsToRanks = {v: k for k, v in rankToRows.items()}
+    rankToCols = {'a':0, 'b':1, 'c':2, 'd':3, 'e':4, 'f':5, 'g':6, 'h':7}
+    colsToFiles = {v:k for k, v in rankToCols.items()}
+
+    def __init__(self, seq):
+        self.seq = seq.lower()
 
     def getChessNotationSeq(self):
         '''
@@ -58,13 +300,18 @@ class Move:
         basically, it will return the tuple(rows, cols) to files like e4
         '''
         return self.colsToFiles[c] + self.rowsToRanks[r]
-
-    def getRankColsTup(self, seq):
-        ''' convert the sequences into tuples e4 to (3, 4)'''
-        return self.rankToCols[seq[0]], self.rankToRows[seq[1]]
     
-    def getChessNotationTup(self, seq):
-        ''' return sequences as tuples e4e5 to (3, 4), (5, 6)'''
-        source_move = self.getRankColsTup(seq[:2])
-        dest_move = self.getRankColsTup(seq[2:])
+    def getRankColsTup(self, move):
+        ''' convert the sequences into tuples e4 to (3, 4) '''
+        file_char=move[0]
+        rank_char=move[1]
+
+        '''cause in 2D array we look first row and then col'''
+        row, col = self.rankToRows[rank_char], self.rankToCols[file_char]
+        return row, col
+    
+    def getChessNotationTup(self):
+        ''' return sequences as tuples e4e5 to (3, 4), (5, 6) '''
+        source_move = self.getRankColsTup(self.seq[:2])
+        dest_move = self.getRankColsTup(self.seq[2:])
         return source_move, dest_move 
--- chessMain1.py
+++ chessMain1.py
@@ -1,12 +1,63 @@
-import chessEngine 
+import chessEngine1
 
+'''Main driver of file'''
 def main():
+    gs = chessEngine1.gamestate()
 
-    
-    RUNNING = True
-    while RUNNING:
-        take_input = input("if you want to exit then type 'Q' ")
-        if take_input == 'Q':
-            RUNNING=False
+    '''description for users'''
+    print()
+    print("******** Chess Game ********")
+    print()
+    print("created by 'shivah chaudhary'")
+    print("enter a chess moves like (e2e4, b1c3, e7e8) \nenter (q, exit) for exit: ")
+    while True:
+
+        '''show which turn (black, white)'''
+        print(f"Turn: {'White' if gs.isWhiteTurn else 'Black'}")
+        print()
+
+        take_input = input("enter a valid chess move: ").lower()
+        print()
+
+        '''we have board like wR, wB, wN but do we need to convert into the numerical array??'''
+        validMoves=gs.getAllValidMoves()
         
-        if 
+        if take_input == 'q' or take_input == 'exit':
+            break
+        
+        if len(take_input) != 4:
+            print("invalid input, please enter a move like e2e4")
+            continue
+
+        ''' if take_input like e4e5 then we have to convert it into tuples'''
+        try:
+            conversion = chessEngine1.Conversion(take_input)
+            source, dest = conversion.getChessNotationTup()
+            # print(source, dest)
+        
+            ''' now we have tuples (3, 4), (5, 6) now we can move in any direction first if we want to valid 
+                later on then we can do by generate legal valid move generation '''
+            move = chessEngine1.Move(source, dest, gs.board)
+            
+            if gs.isPlayerValidMove(move, validMoves):
+                ''' now make the move '''
+                gs.makeMove(move)
+                
+                '''print board condition'''
+                gs.printBoardState()
+        
+        except KeyError:
+            print("Invalid square input. use coordinates like e2e4")
+
+        except Exception as e:
+            print(f"Error: {e}")
+                
+        '''undoing a move'''
+        if take_input == 'Z':
+            gs.undoMove()
+            gs.printBoardState()
+
+
+if __name__ == "__main__":
+    main()
+            
