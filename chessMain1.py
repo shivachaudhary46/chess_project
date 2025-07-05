f83a1991e064e2f1c2317a595afd4b0a51ffc6ec
shivachaudhary46 <shivachaudhary4116@gmail.com>
2025-07-05 16:05:41 +0545

    castling rules completed

--- chessMain1.py
+++ chessMain1.py
@@ -10,8 +10,8 @@ def main():
     print()
     print("created by 'shivah chaudhary'")
     print("enter a chess moves like (e2e4, b1c3, e7e8) \nenter (q, exit) for exit \nenter (z, undo) for undo: ")
+    
     while True:
-
         '''show which turn (black, white)'''
         print(f"Turn: {'White' if gs.isWhiteTurn else 'Black'}")
         print()
@@ -21,21 +21,26 @@ def main():
         print()
 
         '''we have board like wR, wB, wN but do we need to convert into the numerical array??'''
-        validMoves=gs.getValidKingChecks()
-
-        take_input = input("enter a valid chess move: ").lower()
-        print()
+        validMoves = gs.getValidKingChecks()
 
         if len(validMoves) == 0:
-            if not gs.isWhiteTurn :
+            if not gs.isWhiteTurn:
                 print("White won Game!!! \ncongratulation!!!")
                 break
             else:
                 print("Black won Game!!! \ncongratulation!!!")
                 break
 
+        take_input = input("enter a valid chess move: ").lower()
+        print()
+
         if take_input == 'q' or take_input == 'exit':
             break
+        
+        '''Handle undo move'''
+        if take_input == 'z' or take_input == 'undo':
+            gs.undoMove()
+            continue
         
         if len(take_input) != 4:
             print("invalid input, please enter a move like e2e4")
@@ -45,23 +50,21 @@ def main():
         try:
             conversion = chessEngine1.Conversion(take_input)
             source, dest = conversion.getChessNotationTup()
-            # print(source, dest)
-        
-            ''' now we have tuples (3, 4), (5, 6) now we can move in any direction first if we want to valid 
-                later on then we can do by generate legal valid move generation '''
-            move = chessEngine1.Move(source, dest, gs.board)
             
-            if gs.isPlayerValidMove(move, validMoves):
-                ''' now make the move '''
+            # move was generated from the valid moves 
+            move = None
+            for valid_move in validMoves:
+                if (valid_move.startRow == source[0] and valid_move.startCol == source[1] and 
+                    valid_move.endRow == dest[0] and valid_move.endCol == dest[1]):
+                    move = valid_move
+                    break
+            
+            if move:
+                # print(f"Executing move: {move.pieceSource} with isCastle={move.isCastle}")
                 gs.makeMove(move)
-                
-                '''print board condition'''
-                gs.printBoardState()
-
-            '''undoing a move'''
-            if take_input == 'z' or take_input == 'undo':
-                gs.undoMove()
-                gs.printBoardState()
+                # print("Move executed successfully!")
+            else:
+                print("Invalid move! Please try again.")
         
         except KeyError:
             print("Invalid square input. use coordinates like e2e4")
@@ -71,4 +74,3 @@ def main():
                 
 if __name__ == "__main__":
     main()
-            
